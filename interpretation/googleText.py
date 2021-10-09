a1007a409b487921245aa7d571ff8ab628aa0990
barrenger <james@barrenger.org>
2021-10-09 16:56:37 +1000

    convert errors, convert save page in db, googleTxt

---
+++ interpretation/googleText.py
@@ -0,0 +1,24 @@
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types #pip install google.cloud.vision
+
+def getTextArray(path):
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+    return texts
+
+    
