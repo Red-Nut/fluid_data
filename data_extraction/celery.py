f9c6dbd3943ba2468762c7f2cf08ebc3da5ac6a8
barrenger <james@barrenger.org>
2021-12-06 07:55:57 +1000

    minor changes to deployment files

--- data_extraction/celery.py
+++ data_extraction/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'data_extraction.settings.prod')
 
-app = Celery('file_manager')
+app = Celery('data_extraction')
 app.config_from_object('django.conf:settings')
 
 # Load task modules from all rejistered Django app configs.
--- data_extraction/wsgi.py
+++ data_extraction/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/opt/bitnami/fluid_data')
+
+os.environ.setdefault("PYTHON_EGG_CACHE", "/opt/bitnami/fluid_data/egg_cache")
 
 from django.core.wsgi import get_wsgi_application
 
