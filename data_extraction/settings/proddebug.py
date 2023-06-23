6fef755c122fe54c028b78dd2773b7da6c4328b4
Red-Nut <james.barrenger@gmail.com>
2023-06-23 21:16:32 +1000

    additional settings file

---
+++ data_extraction/settings/proddebug.py
@@ -0,0 +1,44 @@
+from data_extraction.settings.base import *
+
+ALLOWED_HOSTS = ['localhost', '10.10.10.10','13.54.13.187','fluiddata.com.au','www.fluiddata.com.au']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
+
+
+#Storage Backend
+USE_S3 = True
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = env('AWS_BUCKET')
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+# Static
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'data_extraction/static/'),
+)
+
+AWS_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'temp/')
+AWS_MEDIA_LOCATION = 'media'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
